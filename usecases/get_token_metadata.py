e1f4c5e3f4eec22a9994d5f0c1f12e6922b9f850
Giovane Costa <gigio.coder@gmail.com>
2021-06-04 18:47:20 -0300

    feat(tx): create token metadata endpoint

---
+++ usecases/get_token_metadata.py
@@ -0,0 +1,16 @@
+from typing import Union
+from gateways.token_gateway import TokenGateway
+
+
+class GetTokenMetadata:
+
+    def __init__(self, token_gateway: Union[TokenGateway, None] = None) -> None:
+        self.token_gateway = token_gateway or TokenGateway()
+
+    def get(self, id: str) -> Union[dict, None]:
+        meta = self.token_gateway.get_token_metadata_from_s3(f"{id}.json")
+
+        if meta:
+            return meta.to_dict()
+
+        return None
