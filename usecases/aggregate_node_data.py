3612009eea838218e1eab373b8cbbb0fefa20116
André Carneiro <andreluizmrcarneiro@gmail.com>
2022-04-01 19:13:46 -0300

    chore: stop diff early

--- usecases/aggregate_node_data.py
+++ usecases/aggregate_node_data.py
@@ -1,12 +1,25 @@
 from typing import Union
 
 from deepdiff import DeepDiff
+from deepdiff.model import DiffLevel
 
 from gateways.node_gateway import NodeGateway
 
 
-class AggregateNodeData:
+class EarlyStopDiff:
+    """ Stop the diff when we find an error
 
+    From the docs example at:
+    https://zepworks.com/deepdiff/5.7.0/custom.html#custom-operators
+    """
+    def match(self, level: DiffLevel) -> bool:
+        return True
+
+    def give_up_diffing(self, level: DiffLevel, diff_instance: DeepDiff) -> bool:
+        return any(diff_instance.tree.values())
+
+
+class AggregateNodeData:
     def __init__(self, node_gateway: Union[NodeGateway, None] = None) -> None:
         self.node_gateway = node_gateway or NodeGateway()
 
@@ -18,7 +31,8 @@ class AggregateNodeData:
             regex_path_to_exclude = r"root\['(nodes|peers)'\]\[\d+\]\['uptime'\]"
             new_dict = new_network.to_dict()
             old_dict = old_network.to_dict()
-            networks_diff = DeepDiff(new_dict, old_dict, ignore_order=True, exclude_regex_paths=regex_path_to_exclude)
+            networks_diff = DeepDiff(new_dict, old_dict, ignore_order=True, exclude_regex_paths=regex_path_to_exclude,
+                                     custom_operators=[EarlyStopDiff()])
 
         if (not old_network) or networks_diff:
             return self.node_gateway.save_network(new_network)
