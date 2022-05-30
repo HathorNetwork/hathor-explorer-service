4f31f354590805229243ed1ac3987516aae48c01
André Abadesso <andre.abadesso@gmail.com>
2022-05-29 21:11:45 -0300

    feat: generalized elastic search utils to be reused

---
+++ utils/elastic_search/transformations/token_balances.py
@@ -0,0 +1,16 @@
+def es_hit_to_result(hit: dict) -> dict:
+    """Gets a unique hit from ElasticSearch and map it to what API client expects.
+
+    :param hit: Raw ElasticSearch hit
+    :type hit: dict
+    """
+    result = {
+        'address': hit['_source']['address'],
+        'token_id': hit['_source']['token_id'],
+        'unlocked_balance': hit['_source']['unlocked_balance'],
+        'locked_balance': hit['_source']['locked_balance'],
+        'total': hit['_source']['total'],
+        'sort': hit['sort']
+    }
+
+    return result
