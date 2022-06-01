95f0a455f7f554de9c5cea99b45ffdc284f0e385
André Abadesso <andre.abadesso@gmail.com>
2022-06-01 00:33:09 -0300

    refactor: attempt o generalize elasticsearch utils

--- utils/elastic_search/elastic_search_utils.py
+++ utils/elastic_search/elastic_search_utils.py
@@ -43,7 +43,7 @@ class ElasticSearchUtils:
     def __init__(self, elastic_index: str):
         self.elastic_index = elastic_index
 
-    def _get_sort_by_complement(self, sort_by: str) -> str:
+    def get_sort_by_complement(self, sortable_fields: List[str], sort_by: str) -> str:
         """ Returns the complement (if any) on sort_by field that is passed to ES
 
         :param sort_by: Which field is currently being used for primary sorting
@@ -52,7 +52,7 @@ class ElasticSearchUtils:
 
         # For sorting purposes, only .keyword must be added as complement
         # Other types (such as long) do not need complement
-        return '.keyword' if SORTABLE_FIELDS_BY_INDEX[self.elastic_index][sort_by] == 'keyword' else ''
+        return '.keyword' if sortable_fields[sort_by] == 'keyword' else ''
 
     def build_search_query(self, search_text: str, sort_by: str, order: str, search_after: List[str]) -> dict:
         """Build the search query that will be sent to ElasticSearch given the parameters provided by API Client.
@@ -71,7 +71,6 @@ class ElasticSearchUtils:
         :param search_after: A list with two entries with information of the next page
         :type search_after: List[str]
         """
-
         # Default sort order, if nothing is passed
         sort_order = DEFAULT_SORT_ORDER_BY_INDEX[self.elastic_index].copy() # Make sure we dont mutate it
 
@@ -83,7 +82,7 @@ class ElasticSearchUtils:
 
         primary_sort_key = {}
 
-        sort_by_complement = self._get_sort_by_complement(sort_by)
+        sort_by_complement = self.get_sort_by_complement(sortable_fields=SORTABLE_FIELDS_BY_INDEX[self.elastic_index], sort_by=sort_by)
         primary_sort_key[sort_by+sort_by_complement] = order
 
         sort_order.remove(sort_by)
@@ -91,7 +90,7 @@ class ElasticSearchUtils:
         tie_break_sort_order = sort_order.pop(0)
         tie_break_sort_key = {}
 
-        sort_by_complement = self._get_sort_by_complement(tie_break_sort_order)
+        sort_by_complement = self.get_sort_by_complement(sortable_fields=SORTABLE_FIELDS_BY_INDEX[self.elastic_index], sort_by=tie_break_sort_order)
         tie_break_sort_key[tie_break_sort_order+sort_by_complement] = 'asc'
 
         body = {
@@ -101,16 +100,20 @@ class ElasticSearchUtils:
                 tie_break_sort_key
             ],
             'index': self.elastic_index,
-            'request_timeout': int(ELASTIC_SEARCH_TIMEOUT)
+            'request_timeout': int(ELASTIC_SEARCH_TIMEOUT),
         }
 
         if search_text:
             fields = SEARCH_TEXT_FIELDS_BY_INDEX[self.elastic_index]
             body['query'] = {
-                'multi_match': {
-                    'query': search_text,
-                    'fields': fields
-                }
+                'bool': {
+                    'must': {
+                        'multi_match': {
+                            'query': search_text,
+                            'fields': fields
+                        },
+                    }
+                },
             }
 
         if search_after:
