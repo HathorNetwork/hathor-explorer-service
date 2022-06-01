9f53c82b4534a97c1eba13ce9daeb10a8c473848
André Abadesso <andre.abadesso@gmail.com>
2022-06-01 00:31:47 -0300

    refactor: handle elasticsearch logic on gateway

--- gateways/token_balances_api_gateway.py
+++ gateways/token_balances_api_gateway.py
@@ -1,8 +1,16 @@
 from typing import List, Optional
 from elasticsearch import Elasticsearch
 from gateways.clients.elastic_search_client import ElasticSearchClient
-from common.configuration import ELASTIC_TOKEN_BALANCES_INDEX
+from utils.elastic_search.elastic_search_utils import ElasticSearchUtils
+from common.configuration import ELASTIC_RESULTS_PER_PAGE, ELASTIC_SEARCH_TIMEOUT, ELASTIC_INDEX, ELASTIC_TOKEN_BALANCES_INDEX
 
+SORTABLE_FIELDS = {
+    'address': 'keyword',
+    'token_id': 'keyword',
+    'unlocked_balance': 'long',
+    'locked_balance': 'long',
+    'total': 'long',
+}
 
 class TokenBalancesApiGateway:
     """ Gateway to interact with the Hathor ElasticSearch cluster with the token balances index
@@ -14,7 +22,78 @@ class TokenBalancesApiGateway:
     ) -> None:
         self.elastic_search_client = ElasticSearchClient(elastic_index=ELASTIC_TOKEN_BALANCES_INDEX, client=elastic_search_client)
 
-    def get_token_balances(self, search_text: str, sort_by: str, order: str, search_after: List[str]) -> dict:
+    def get_token_information(self, token_id: str) -> dict:
+        """Retrieve total number of addresses and transactions for a given token_id
+        """
+
+        body = {
+            'size': 0,
+            'query': {
+                'match': {
+                    'token_id': token_id
+                }
+            },
+            'aggs': {
+                'address_count': {
+                    'value_count': {
+                        'field': 'address.keyword'
+                    }
+                },
+                'transaction_sum': {
+                    'sum': {
+                        'field': 'transactions'
+                    }
+                }
+            }
+        }
+
+        response = self.elastic_search_client.run(body)
+
+        return {
+            'transactions': response['aggregations']['transaction_sum']['value'],
+            'addresses': response['aggregations']['address_count']['value']
+        }
+
+    def get_token_balances(self, token_id: str, search_text: str, sort_by: str, order: str, search_after: List[str]) -> dict:
         """Retrieve all token balances that match user's query
         """
-        return self.elastic_search_client.make_query(search_text, sort_by, order, search_after)
+
+        elastic_search_utils = ElasticSearchUtils(elastic_index=ELASTIC_TOKEN_BALANCES_INDEX)
+        sort_order = ['total', 'unlocked_balance', 'locked_balance', 'address']
+
+        if not sort_by:
+            sort_by = sort_order[0]
+
+        if not order:
+            order = 'asc'
+
+        primary_sort_key = {}
+
+        sort_by_complement = elastic_search_utils.get_sort_by_complement(sortable_fields=SORTABLE_FIELDS, sort_by=sort_by)
+        primary_sort_key[sort_by+sort_by_complement] = order
+
+        sort_order.remove(sort_by)
+
+        tie_break_sort_order = sort_order.pop(0)
+        tie_break_sort_key = {}
+
+        sort_by_complement = elastic_search_utils.get_sort_by_complement(sortable_fields=SORTABLE_FIELDS, sort_by=tie_break_sort_order)
+
+        tie_break_sort_key[tie_break_sort_order+sort_by_complement] = 'asc'
+
+        body = {
+            'size': int(ELASTIC_RESULTS_PER_PAGE) + 1,  # Last element is to check if there is next page.
+            'sort': [
+                primary_sort_key,
+                tie_break_sort_key
+            ],
+            'index': ELASTIC_TOKEN_BALANCES_INDEX,
+            'request_timeout': int(ELASTIC_SEARCH_TIMEOUT),
+            'query': {
+                'match': {
+                    'token_id': token_id
+                }
+            }
+        }
+
+        return elastic_search_utils.treat_response(self.elastic_search_client.run(body))
