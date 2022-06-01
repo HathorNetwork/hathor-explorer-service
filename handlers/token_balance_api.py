2935b308ea19779544cc8530b8d87ccca5c10708
André Abadesso <andre.abadesso@gmail.com>
2022-06-01 00:32:09 -0300

    feat: added token balance information handler

--- handlers/token_balance_api.py
+++ handlers/token_balance_api.py
@@ -18,24 +18,46 @@ def get_token_balances(
 
     token_balances_api = token_balances_api or TokenBalancesApi()
 
-    search_text = event.query.get("search_text") or ""
-    sort_by = event.query.get("sort_by") or ""
-    order = event.query.get("order") or "asc"  # asc/desc
-    search_after = event.query.get("search_after") or ""
+    token_id = event.query.get('token_id') or "00"
+    search_text = event.query.get('search_text') or ""
+    sort_by = event.query.get('sort_by') or ""
+    order = event.query.get('order') or 'asc'  # asc/desc
+    search_after = event.query.get('search_after') or ''
     search_after_list = []
 
     if search_after:
         search_after_list = list(search_after.split(","))
 
         if len(search_after_list) != 2:
-            raise ApiError("Invalid search_after parameter")
+            raise ApiError('Invalid search_after parameter')
 
-    response = token_balances_api.get_token_balances(search_text, sort_by, order, search_after_list)
+    response = token_balances_api.get_token_balances(token_id, search_text, sort_by, order, search_after_list)
 
     return {
-        "statusCode": 200,
-        "body": json.dumps(response or {}),
-        "headers": {
-            "Content-Type": "application/json"
+        'statusCode': 200,
+        'body': json.dumps(response or {}),
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+
+@ApiGateway()
+def get_token_information(
+    event: ApiGatewayEvent,
+    _context: LambdaContext,
+    token_balances_api: Optional[TokenBalancesApi] = None
+) -> dict:
+    """Get token information from a given token_id
+    """
+
+    token_balances_api = token_balances_api or TokenBalancesApi()
+    token_id = event.query.get('token_id') or '00'
+    response = token_balances_api.get_token_information(token_id)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response or {}),
+        'headers': {
+            'Content-Type': 'application/json'
         }
     }
