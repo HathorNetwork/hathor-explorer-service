1a4b2254d1700ae3a8dddee909ba33393202e463
Luis Helder <luislhl@gmail.com>
2023-11-06 18:19:43 -0300

    ci: tests for network statistics api

--- gateways/network_statistics_api_gateway.py
+++ gateways/network_statistics_api_gateway.py
@@ -8,9 +8,9 @@ from gateways.clients.elastic_search_client import ElasticSearchClient
 
 
 class NetworkStatisticsApiGateway:
-    def __init__(self, elastic_search_client: Optional[Elasticsearch] = None) -> None:
-        self.elastic_search_client = ElasticSearchClient(
-            elastic_index=ELASTIC_TX_INDEX, client=elastic_search_client
+    def __init__(self, elastic_search_client: Optional[ElasticSearchClient] = None) -> None:
+        self.elastic_search_client = elastic_search_client or ElasticSearchClient(
+            elastic_index=ELASTIC_TX_INDEX
         )
 
     def get_transaction_statistics(self) -> dict:
---
+++ tests/unit/usecases/test_network_statistics_api.py
@@ -0,0 +1,39 @@
+from unittest.mock import MagicMock, patch
+from elasticsearch import exceptions
+from common.errors import ApiError
+
+from pytest import fixture, raises
+
+from usecases.network_statistics_api import NetworkStatisticsApi
+
+
+class TestNetworkStatisticsApi:
+    @fixture
+    def gateway(self):
+        return MagicMock()
+
+    @patch("usecases.network_statistics_api.logger")
+    def test_get_basic_statistics(self, logger, gateway):
+        api = NetworkStatisticsApi(network_statistics_api_gateway=gateway)
+        gateway.get_transaction_statistics.return_value = {"foo": "bar"}
+        result = api.get_basic_statistics()
+        gateway.get_transaction_statistics.assert_called_once()
+        assert result == {"foo": "bar"}
+
+    @patch("usecases.network_statistics_api.logger")
+    def test_get_basic_statistics_gateway_error(self, logger, gateway):
+        api = NetworkStatisticsApi(network_statistics_api_gateway=gateway)
+        gateway.get_transaction_statistics.side_effect = exceptions.ApiError("gateway_error", MagicMock(), MagicMock())
+        with raises(ApiError):
+            api.get_basic_statistics()
+        gateway.get_transaction_statistics.assert_called_once()
+        logger.error.assert_called_once()
+
+    @patch("usecases.network_statistics_api.logger")
+    def test_get_basic_statistics_internal_error(self, logger, gateway):
+        api = NetworkStatisticsApi(network_statistics_api_gateway=gateway)
+        gateway.get_transaction_statistics.side_effect = exceptions.TransportError("internal_error")
+        with raises(ApiError):
+            api.get_basic_statistics()
+        gateway.get_transaction_statistics.assert_called_once()
+        logger.error.assert_called_once()
